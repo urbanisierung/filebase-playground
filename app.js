01cf07e384350a8bf104a809b3e426cede27a2d4
Adam <i@adamurban.de>
2021-07-04 08:39:30 +0200

    :tada:

---
+++ app.js
@@ -0,0 +1,26 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3({endpoint: 'https://s3.filebase.com', signatureVersion: 'v4'});
+
+s3.listBuckets(function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    var params = {
+      Body: 'Hello, world!',
+      Bucket: data['Buckets'][0]['Name'],
+      Key: 'exampleobject',
+      ContentType: 'text/plain',
+      Metadata: {
+       'metadata1': 'value1',
+       'metadata2': 'value2'
+      }
+    };
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        console.log(data);
+      }
+    });
+  }
+});
